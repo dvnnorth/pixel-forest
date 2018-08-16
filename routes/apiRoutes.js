5e96ceed9b2365b813e1764dd17b7a928fb40c07
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-08-15 21:13:25 -0500

    login page with signup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,49 @@
 var db = require("../models");
+var firebase = require("firebase");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+var config = {
+    apiKey: "AIzaSyA6FqIL02M3IGmtfWPNFdXTV8Aw_FIY6ZQ",
+    authDomain: "sweetpea-74522.firebaseapp.com",
+    databaseURL: "https://sweetpea-74522.firebaseio.com",
+    projectId: "sweetpea-74522",
+    storageBucket: "sweetpea-74522.appspot.com",
+    messagingSenderId: "1006334747003"
+};
+firebase.initializeApp(config);
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+var auth = firebase.auth();
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+
+module.exports = function (app) {
+    // Get all examples
+    app.get("/api/examples", function (req, res) {
+        db.Example.findAll({}).then(function (dbExamples) {
+            res.json(dbExamples);
+        });
     });
-  });
+
+    // Create a new example
+    app.post("/api/examples", function (req, res) {
+        db.Example.create(req.body).then(function (dbExample) {
+            res.json(dbExample);
+        });
+    });
+    // Delete an example by id
+    app.delete("/api/examples/:id", function (req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+            res.json(dbExample);
+        });
+    });
+    // Create a new example
+    app.post("/login/signup", function (req, res) {
+        console.log(req.body);
+        //   pass info to authentication engine
+        firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+
+    });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,10 @@
 var db = require("../models");
+var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,8 +13,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +22,13 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  
+  // test page to test route
+  app.get("/login.html", function (req, res) {
+    res.sendfile(path.join(__dirname, "../login.html"));
+  });
+  
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
