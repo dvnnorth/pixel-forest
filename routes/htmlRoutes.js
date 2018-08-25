166ae9686dfb6a92b8ac8ef42d0b4b867db10ed0
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-08-24 22:52:43 -0500

    Committing current state of route code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -89,46 +89,6 @@ module.exports = function (app, firebase, fbAdmin) {
         res.render('post');
     });
 
-    app.get('/share/:token', function (req, res) {
-        // Get the User a Viewer can see
-        // Render their profile page without post functionality
-        db.Viewers.findOne(
-            {
-                where: {
-                    accessToken: req.params.token
-                }
-            }
-        )
-            .then(function (viewer) {
-                db.Users.findOne(
-                    {
-                        where: {
-                            id: viewer.UserId
-                        }
-                    },
-                    {
-                        include: [db.Users]
-                    }
-                )
-                    .then(function (user) {
-                        db.Posts.findAll(
-                            {
-                                where: {
-                                    UserId: user.id
-                                }
-                            },
-                            {
-                                include: [db.Users]
-                            }
-                        )
-                            .then(function (posts) {
-                                console.log(posts);
-                                res.render('share', posts);
-                            });
-                    });
-            });
-    });
-
     // Render 404 page for any unmatched routes
     app.get('*', function (req, res) {
         res.render('404');
