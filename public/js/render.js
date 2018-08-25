0af490019dc52dcfbf71908c781cf5d4e476bef8
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2018-08-24 19:21:19 -0500

    Committing current status on api routes

--- public/js/render.js
+++ public/js/render.js
@@ -15,17 +15,48 @@ $(function () {
 
                 // Render images here
 
-
-                console.log(content);
                 content.forEach(function (post) {
                     // This builds the images with a close button that will delete each image when clicked
                     // On click event for deleting above. Use data-deleteid attribute of button
                     var newImg = $('<img>')
+                        .attr('class', 'profilePhoto')
                         .attr('src', post.pictureUrl)
                         .attr('data-photoid', post.id)
                         .attr('width', 260)
                         .attr('height', 260)
                     $("#postsDiv").append(newImg);
+                });
+
+                // On Click listener for displaying image modal
+
+                $('.profilePhoto').on('click', function (event) {
+                    var $this = $(this);
+                    var photoId = $(this).data('photoid');
+                    var route = '/api/profile/post/' + photoId;
+                    $.ajax({
+                        method: 'GET',
+                        url: route,
+                        async: true,
+                        headers: {
+                            token: sessionStorage.getItem('token'),
+                            id: sessionStorage.getItem('id')
+                        }
+                    })
+                        .then(function (post) {
+                            var $src = $this.attr('src');
+                            var $modal = $('#exampleModalCenter');
+                            var $wrapper = $();
+                            var $title = $()
+                            var $image = $('<img>')
+                                .attr('class', 'modalPhoto')
+                                .attr('src', post.pictureUrl)
+                                .attr('data-photoid', post.id)
+                                .attr('width', 260)
+                                .attr('height', 260);
+                            $('#exampleModalCenter .modal-content')
+                                .append($image);
+                            $modal.modal('show');
+                        });
                 });
             })
             .fail(function () {
